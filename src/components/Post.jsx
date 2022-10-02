b46f746a8a82b156682559133551b7ab9868746a
Darren Cal <darrencal64@gmail.com>
2022-10-02 11:31:59 -0600

    Add posts to feed

---
+++ src/components/Post.jsx
@@ -0,0 +1,60 @@
+import React from 'react';
+import { Favorite, FavoriteBorder, MoreVert, Share } from '@mui/icons-material';
+import {
+  Avatar,
+  Card,
+  CardActions,
+  CardContent,
+  CardHeader,
+  CardMedia,
+  Checkbox,
+  IconButton,
+  Typography,
+} from '@mui/material';
+
+const Post = () => {
+  return (
+    <Card sx={{ margin: 5 }}>
+      <CardHeader
+        avatar={
+          <Avatar sx={{ bgcolor: 'red' }} aria-label="card">
+            J
+          </Avatar>
+        }
+        action={
+          <IconButton aria-label="settings">
+            <MoreVert />
+          </IconButton>
+        }
+        title="John Doe"
+        subheader="September 14, 2016"
+      />
+      <CardMedia
+        component="img"
+        height="20%"
+        image="https://images.pexels.com/photos/4534200/pexels-photo-4534200.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+        alt="Lorem ipsum"
+      />
+      <CardContent>
+        <Typography variant="body2" color="text.secondary">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor
+          consectetur voluptatum natus porro, rem delectus itaque provident.
+          Assumenda, tempore libero!
+        </Typography>
+      </CardContent>
+      <CardActions disableSpacing>
+        <IconButton aria-label="add to favorites">
+          <Checkbox
+            icon={<FavoriteBorder />}
+            checkedIcon={<Favorite sx={{ color: 'red' }} />}
+          />
+        </IconButton>
+        <IconButton aria-label="share">
+          <Share />
+        </IconButton>
+      </CardActions>
+    </Card>
+  );
+};
+
+export default Post;
